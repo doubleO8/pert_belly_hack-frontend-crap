dd6060c41812cb80d1f92a1c039d3d84944e4b6b
doubleO8 <wb008@hdm-stuttgart.de>
2018-04-04 20:39:33 +0200

    update package name, epoch

---
+++ pert_belly_hack_frontend_crap/defaults.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from . import __version__
+
+#: meta data for opkg
+PACKAGE_META = {
+    "package": "pert-belly-hack-frontend-crap",
+    "upstream_version": __version__,
+    "epoch": 2,
+    "target_root_path": "OpenWebif"
+}
