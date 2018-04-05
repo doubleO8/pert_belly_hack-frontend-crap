f0ce30c00bb8041501a96df23759402b50a6d6b5
doubleO8 <wb008@hdm-stuttgart.de>
2018-04-05 20:07:00 +0200

    updated README and packaging meta data

--- pert_belly_hack_frontend_crap/defaults.py
+++ pert_belly_hack_frontend_crap/defaults.py
@@ -7,7 +7,10 @@ PACKAGE_META = {
     "package": "pert-belly-hack-frontend-crap",
     "upstream_version": __version__,
     "epoch": 2,
-    "target_root_path": "OpenWebif"
+    "target_root_path": "OpenWebif",
+    "description": "frontend component providing the outdated Browser UI",
+    "depends": 'python-json, python-cheetah, python-unixadmin, python-misc, python-twisted-web, python-pprint, python-compression, python-ipaddress, pert-belly-hack-backend',
+    "conflicts": "enigma2-plugin-extensions-openwebif",
 }
 
 TAG_PATH_REL = 'public/tag_frontend-crap.json'
